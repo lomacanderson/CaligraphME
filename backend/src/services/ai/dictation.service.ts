ca65f135737ad1f4f5ede2295518d810d4882fc1
jwchehab <jwchehab@gmail.com>
2025-10-18 19:10:33 -0700

    Added dictation service.

---
+++ backend/src/services/ai/dictation.service.ts
@@ -0,0 +1,40 @@
+import { ElevenLabsClient } from '@elevenlabs/elevenlabs-js';
+import * as dotenv from 'dotenv';
+import { createWriteStream } from 'fs';
+import { Readable } from 'stream'; 
+
+dotenv.config();
+
+const ELEVENLABS_API_KEY = process.env.ELEVENLABS_API_KEY;
+
+const dictationService = new ElevenLabsClient({
+  apiKey: ELEVENLABS_API_KEY,
+});
+
+export const textToAudio = async (
+    text: string, 
+    fileName: string, 
+    voice: string = 'JBFqnCBsd6RMkjVDRZzb'): 
+    Promise<string> => {
+  return new Promise<string>(async (resolve, reject) => {
+    try {
+      const audio = await dictationService.textToSpeech.convert(voice, {
+        modelId: 'eleven_multilingual_v2',
+        text,
+        outputFormat: 'mp3_44100_128',
+        // Optional voice speed setting
+        voiceSettings: {
+          speed: 1.0,
+        },
+      });
+
+      const fileStream = createWriteStream(fileName);
+      Readable.fromWeb(audio).pipe(fileStream);
+
+      fileStream.on('finish', () => resolve(fileName)); // Resolve with the fileName
+      fileStream.on('error', reject);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
